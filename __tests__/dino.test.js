1ce07abc691aec426254a1f6b60ddc8356584dd1
unknown <macjay314@gmail.com>
2020-04-22 10:47:06 -0700

    Revise initial API test

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -1,12 +1,28 @@
 import {Dino} from '../src/dino'
 
-describe('Dino-Ipsum Calls', () => {
+describe('Initial Dino-Ipsum Calls', () => {
   let dino;
+  // let promise;
+  let request;
+  // let url;
 
   beforeEach(() => {
     dino = new Dino();
+
   });
 
-  test('Should receive a paragraph from dino-ipsum API', () => {});
-  dino.requestTest();
+  test('Should receive a paragraph from dino-ipsum API', () => {
+    let result;
+    request = new XMLHttpRequest();
+    request.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&words=15&paragraphs=1');
+    request.onload = function(){
+      if (this.status === 200){
+        result = "Request Successful";
+      } else {
+        result = "Request Failed";
+      }     
+    }
+    expect(result).toEqual("Request Successful");
+  });
+
 });
