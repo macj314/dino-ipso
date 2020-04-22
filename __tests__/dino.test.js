7be5392a87427691cd4ca7ab55490d86df98cf25
unknown <macjay314@gmail.com>
2020-04-22 14:19:00 -0700

    Continue to troubleshoot api request

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -14,7 +14,16 @@ describe('Initial Dino-Ipsum Calls', () => {
   test('Should get a successful request from dino-ipsum api', () => {
     (async () => {
       let result = await dino.dinoRequest();
-      expect(result).toEqual(true);
+      expect(getElements(result)).toEqual(true);
     })();
+    function getElements(result){
+      if (result){
+        console.log("We got the Dino Ipsum!")
+        return true;
+      } else {
+        console.log("There was an error getting your response")
+        return false;
+      }
+    }
   });
 });
--- src/dino.js
+++ src/dino.js
@@ -2,6 +2,7 @@ export class Dino{
   constructor(paragraphs){
     this.paragraphs = paragraphs
   }
+  
   async dinoRequest() {
     console.log("dinoRequest() started")
     try {
@@ -9,12 +10,15 @@ export class Dino{
       let result;
       let response = await fetch('http://dinoipsum.herokuapp.com/api/?format=json');
       if (response.ok && response.status == 200) {
+        console.log("response.ok")
         result = true;
       } else {
+        console.log("else")
         result = false;
       }
       return result;
     } catch {
+      console.log("Catch")
       return false;
     }
   }
