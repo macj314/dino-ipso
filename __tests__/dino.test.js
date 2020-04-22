0b3fc3567792a421c1b4de882654ad230715e094
unknown <macjay314@gmail.com>
2020-04-22 11:42:36 -0700

    Move API request to dino.js

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -2,27 +2,19 @@ import {Dino} from '../src/dino'
 
 describe('Initial Dino-Ipsum Calls', () => {
   let dino;
-  // let promise;
+  let promise;
   let request;
-  // let url;
+  let url;
 
   beforeEach(() => {
     dino = new Dino();
 
   });
 
-  test('Should receive a paragraph from dino-ipsum API', () => {
-    let result;
-    request = new XMLHttpRequest();
-    request.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=json&words=15&paragraphs=1');
-    request.onload = function(){
-      if (this.status === 200){
-        result = "Request Successful";
-      } else {
-        result = "Request Failed";
-      }     
-    }
-    expect(result).toEqual("Request Successful");
+  test('Should get a successful request from dino-ipsum api', () => {
+    (async () => {
+      let result = await dino.dinoRequest();
+      expect(result).toEqual(true);
+    })();
   });
-
 });
--- src/dino.js
+++ src/dino.js
@@ -1,5 +1,19 @@
 export class Dino{
   constructor(paragraphs){
-    this.paragraphs = paragraphs;
+    this.paragraphs = paragraphs
+  }
+  async dinoRequest() {
+    try {
+      let result;
+      let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json`);
+      if (response.ok && response.status == 200) {
+        result = true;
+      } else {
+        result = false;
+      }
+      return result;
+    } catch {
+      return false;
+    }
   }
 }
