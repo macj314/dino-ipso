b086fd0b78bc9e1fe840e2660575a2fc837f89e2
unknown <macjay314@gmail.com>
2020-04-22 15:19:53 -0700

    Add second test. This test should params to the dino-ipsum api and get a corresponding response.

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -7,14 +7,21 @@ describe('Initial Dino-Ipsum Calls', () => {
   let url;
 
   beforeEach(() => {
-
+    dino = new Dino();
   });
 
   test('Should get a successful request from dino-ipsum api', () => {
     (async () => {
-      dino = new Dino();
-      let result = await dino.dinoRequest();
-      expect(result).toEqual(true);
+      request = await dino.dinoRequestTest();
+      expect(request).toEqual(true);
+    })();
+  });
+
+  test('Should get a request from dino ipsum with the user params of: 2 paragraphs, 10 words.', () => {
+    url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=10';
+    (async () => {
+      request = await dino.dinoRequest(url);
+      expect(request).toEqual(true);
     })();
   });
 });
--- src/dino.js
+++ src/dino.js
@@ -1,15 +1,28 @@
 const fetch = require("node-fetch");
 export class Dino{
   constructor(paragraphs){
-    this.paragraphs = paragraphs
+    this.paragraphs = [paragraphs]
   }
 
-  async dinoRequest() {
-    console.log("dinoRequest() started")
+  async dinoRequestTest() {
     try {
-      console.log("Start of try block")
       let response = await fetch('http://dinoipsum.herokuapp.com/api/?format=json');
       if (response.ok && response.status == 200) {
+        response = true;
+      } else {
+        response = false;
+      }
+      return response;
+    } catch(error) {
+      return false;
+    }
+  }
+
+  async dinoRequest(url) {
+    try {
+      let response = await fetch(url);
+      if (response.ok && response.status == 200) {
+        await console.log(response);
         response = true;
       } else {
         response = false;
