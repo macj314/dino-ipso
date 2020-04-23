50e586f31b9e61b69e0a7eabcb74c4a355a777f3
unknown <macjay314@gmail.com>
2020-04-23 11:27:55 -0700

    Convert response to .json (even though the response is in json format). This fixes the response errors, and the program can receive dino-ipsum values

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -11,29 +11,26 @@ describe('Initial Dino-Ipsum Calls', () => {
     dino = new Dino();
   });
 
-  test('Should get a successful request from dino-ipsum api', () => {
-    (async () => {
-      response = await dino.dinoRequestTest();
-      
-      expect(response).toEqual(true);
-    })();
+  test('Should get a successful request from dino-ipsum api', async () => {
+    response = await dino.dinoRequestTest();
+    console.log(response);
+    expect(response).toEqual(true);
   });
 
-  // test('Should get a request from dino ipsum with the user params of: 2 paragraphs, 10 words.', async() => {
-  //   url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=10';   
-  //   (async () => {
-  //     response = await dino.dinoRequest();
-  //     expect(response).toEqual(true);
-  //   })();
-  //   function getElements(response) {
-  //     if (response) {
-  //       answer = true;
-  //     } else {
-  //       answer = false;
-  //     }
-  //     return answer;
-  //   }
-  // });
+  test('Should get a request from dino ipsum with the user params of: 2 paragraphs, 10 words.', async () => {
+    url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=10';   
+    response = await dino.dinoRequest();
+    expect(getElements(response)).toEqual(true);
+    expect
+    function getElements(response) {
+      if (response) {
+        answer = true;
+      } else {
+        answer = false;
+      }
+      return answer;
+    }
+  });
 
   
 });
--- src/dino.js
+++ src/dino.js
@@ -7,48 +7,37 @@ export class Dino{
  
   async dinoRequestTest() {
     try {
+      // console.log("1");
       let response = await fetch('http://dinoipsum.herokuapp.com/api/?format=json');
-      console.log(response);
+      // console.log(response);
       if (response.ok && response.status == 200) {
-        
-        response = true;
+        // console.log(response);
+        response = await response.json();
+        // console.log("3");
       } else {
+        // console.log("4");
         response = false;
       }
+      // console.log("5");
       return response;
     } catch(err) {
-      console.log(err.name);
+      // console.log(err.name);
       return false;
     }
   }
 
-  async dinoRequest() {
-    try {
-      let response = await fetch('http://dinoipsum.herokuapp.com/api');
-      let jsonifiedResponse;
-      if (response.ok && response.status == 200) {        
-        jsonifiedResponse = await response.json();
-      } else {
-        jsonifiedResponse = false;
-      }
-      return jsonifiedResponse
-    } catch(error) {
-      return false;
-    }
-  }
-
-  async getWeatherByCity(city) {
-    try {
-      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-      let jsonifiedResponse;
-      if (response.ok && response.status == 200) {
-        jsonifiedResponse = await response.json();
-      } else {
-        jsonifiedResponse = false;
-      }
-      return jsonifiedResponse;
-    } catch {
-      return false;
-    }
-  }
+  // async dinoRequest() {
+  //   try {
+  //     let response = await fetch('http://dinoipsum.herokuapp.com/api');
+  //     let jsonifiedResponse;
+  //     if (response.ok && response.status == 200) {        
+  //       jsonifiedResponse = await response.json();
+  //     } else {
+  //       jsonifiedResponse = false;
+  //     }
+  //     return jsonifiedResponse
+  //   } catch(error) {
+  //     return false;
+  //   }
+  // }
 }
