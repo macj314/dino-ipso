e86930dfc911ed9e3c08c877b9ef73c50b2331c4
unknown <macjay314@gmail.com>
2020-04-23 13:49:59 -0700

    Finish 3rd test

--- __tests__/dino.test.js
+++ __tests__/dino.test.js
@@ -8,7 +8,7 @@ describe('Initial Dino-Ipsum Calls', () => {
 
   beforeEach(() => {
     dino = new Dino();
-    url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=10'; 
+    url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=2&words=5'; 
   });
 
   test('Should get a successful request from dino-ipsum api (which is an array of 10 arrays)', async () => {
@@ -16,21 +16,25 @@ describe('Initial Dino-Ipsum Calls', () => {
     expect(response).toHaveLength(10);
   });
 
-  test('Should get a request from dino ipsum with the user params of: 2 paragraphs, 10 words.', async () => { 
+  test('Should get a request from dino ipsum with the user params of: 2 paragraphs, 5 words', async () => { 
     response = await dino.dinoRequest(url);
     expect(response).toHaveLength(2); 
-    expect(response[0]).toHaveLength(10);
+    expect(response[0]).toHaveLength(5);
   });  
 
-  test('Should ', async () => {    
+  test('Should display response contents to the console in an easier to read form', async () => {    
     response = await dino.dinoRequest(url);
+    text = '';
     let responseL = response.length;
     for(let i = 0; i < responseL; i++){
       let paragraphArrayL = response[i].length;
-      for(let j = 0; j < paragraphArrayL; j++){
-        text += response[i][j];
+      for(let j = 1; j < paragraphArrayL; j++){
+        text += response[i][j] + " ";
       }
+      text += "|----endPara----| "
     }
+    console.table(response);
+    console.table(response[0])
     console.log(text);
     expect(response).toHaveLength(2);
   });
